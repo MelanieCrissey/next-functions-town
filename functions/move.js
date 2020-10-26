0f6ad2ab7e3999a23e0a5e045f5bb8c30279bdcd
Melanie Crissey <melaniecrissey@gmail.com>
2020-10-26 18:02:18 -0400

    Update example

--- functions/move.js
+++ functions/move.js
@@ -1,10 +1,10 @@
 // functions/move.js
-exports.handler = async function(event, context) {
-  return {
-      statusCode: 200,
-      body: JSON.stringify({message: "Gotta move on"})
-  };
-}
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: "Gotta move on"
+  });
+};
 
 // GET localhost:8888/.netlify/functions/move
 // → Gotta move on
